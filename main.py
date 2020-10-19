b4dfc8f005cc6eb069dcb63d7b3f99190749ee8e
Tal <taldokh@gmail.com>
2020-10-19 14:33:51 +0300

    detects faces on live camera

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('avengers3.jpg')
+webcam = cv2.VideoCapture(0)
+flag = True
+while flag:
+    successful_frame_read, frame = webcam.read()
 
-gratscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-face_coordinates = trained_face_data.detectMultiScale(gratscaled_img)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-cv2.imshow('somthing', img)
-cv2.waitKey()
+    cv2.imshow('FaceDetector', frame)
+    key = cv2.waitKey(1)
+    if(key == 81 or key == 113):
+        flag = False
+
+webcam.release()
 
 print('Code Completed')
 
